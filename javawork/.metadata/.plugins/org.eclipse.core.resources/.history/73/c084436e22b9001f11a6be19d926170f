eb92a4baff729b2a631fea156e58833bbedebe6f
lepe99 <kr_@kakao.com>
2024-12-13 16:18:56 +0900

    123

---
+++ javawork/.metadata/.plugins/org.eclipse.core.resources/.history/73/c084436e22b9001f11a6be19d926170f
@@ -0,0 +1,10 @@
+package boj1_9;
+//
+public class Boj4a05 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- javawork/NaverJavaProject/src/boj1_9/Boj4a05.java
+++ javawork/NaverJavaProject/src/boj1_9/Boj4a05.java
@@ -1,8 +1,20 @@
 package boj1_9;
-//
+
+import java.util.Scanner;
+
+//10810	
 public class Boj4a05 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		sc.nextLine();
+		int[] arr = new int[n + 1];
+		
+		for(int i=1; i<=m; i++) {
+			String[] val = sc.nextLine().split(" ");
+		}
 		
 
 	}
