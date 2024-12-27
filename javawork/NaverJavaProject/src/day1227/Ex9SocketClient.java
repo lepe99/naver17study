d847bb84b425725753dfd9c588597037fdd91afb
lepe99 <kr_@kakao.com>
2024-12-27 14:39:27 +0900

    20241227

---
+++ javawork/NaverJavaProject/src/day1227/Ex9SocketClient.java
@@ -0,0 +1,20 @@
+package day1227;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Ex9SocketClient {
+
+	public static void main(String[] args) {
+		String ip = "192.168.0.26"; // 강사pc ip
+		Socket socket = null;
+		try {
+			socket=new Socket(ip, 6000);
+			System.out.println("서버에 접속 성공");
+		} catch (IOException e) {
+			System.out.println("서버에 접속 실패:"+e.getMessage());
+		}
+
+	}
+
+}
